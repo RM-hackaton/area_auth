0b830a87e73d3d1017c758836c50d1990f2ceeb1
through-your-tears <warstream55@gmail.com>
2022-07-23 01:36:48 +0300

    deployment update

--- rmauth/rmauth/settings.py
+++ rmauth/rmauth/settings.py
@@ -84,8 +84,12 @@ CORS_ALLOW_CREDENTIALS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
