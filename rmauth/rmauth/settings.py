1ce068d40161e5b5c01d12b940c4dfda6120e7e8
through-your-tears <warstream55@gmail.com>
2022-07-23 02:08:05 +0300

    deployment update

--- rmauth/rmauth/settings.py
+++ rmauth/rmauth/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db',
-        'USER': 'myuser',
+        'USER': 'my_user',
         'PASSWORD': 'mysecretpassword',
         'HOST': '127.0.0.1',
         'PORT': '5432'
